cd459f93a19356f24aa820516b849ab4794050e0
Coltzy <coltzy@hotmail.com>
2022-06-01 00:21:38 +0100

    Updated exports

--- src/index.js
+++ src/index.js
@@ -1,4 +1,24 @@
 module.exports = {
   /* Spotify Client */
   Spotify: require('./Spotify.js'),
+
+  /* Managers */
+  AlbumManager: require('./managers/Album.js'),
+  ArtistManager: require('./managers/Artist.js'),
+  AudioManager: require('./managers/Audio.js'),
+  CategoryManager: require('./managers/Categories.js'),
+  EpisodeManager: require('./managers/Episode.js'),
+  PlayerManager: require('./managers/Player.js'),
+  PlaylistManager: require('./managers/Playlist.js'),
+  ShowManager: require('./managers/Show.js'),
+  TrackManager: require('./managers/Show.js'),
+
+  /* Structures */
+  Album: require('./structures/Album.js'),
+  Artist: require('./structures/Artist.js'),
+  Audio: require('./structures/Audio.js'),
+  Episode: require('./structures/Episode.js'),
+  Playlist: require('./structures/Playlist.js'),
+  Show: require('./structures/Show.js'),
+  Track: require('./structures/Track.js'),
 };
